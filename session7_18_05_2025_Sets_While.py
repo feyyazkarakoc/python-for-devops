39943e69eb5ef7c3ed42913bd9e4ac4c8ff424a8
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2025-05-18 23:55:07 +0300

    set

---
+++ session7_18_05_2025_Sets_While.py
@@ -0,0 +1,7 @@
+
+# Sets
+# Türkçesi kümeler
+# Unique değerler içeren (tekrarlı elemanlar kabul etmez) (unique), sıralaması olmayan (unordered), indexlenmeyen (indeksle erişilemez), mutable bir veri yapısıdır
+# {} ile set() metodu ile oluşturulur
+# set() Fonksiyonu Python’daki yerleşik (built-in) bir fonksiyondur. Herhangi bir iterable'ı (liste, string, tuple vs.) alır ve bir kümeye (set) çevirir.
+print(set("hello")) # {'e', 'o', 'l', 'h'} # tekrar eden l'yi almadı, sıralama yok
